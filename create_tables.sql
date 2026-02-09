e59010f8329a732d5011a3b5c47d47dd7dc96a1b
Isabella Victoria <isacdd.04@gmail.com>
2026-02-08 21:03:15 -0300

    Update create_tables.sql
    aprimoramento do código;

--- create_tables.sql
+++ create_tables.sql
@@ -3,16 +3,16 @@ USE SuporteDB;
 
 CREATE TABLE clientes (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100),
-    empresa VARCHAR(100)
+    nome VARCHAR(100) NOT NULL,
+    empresa VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE chamados (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    cliente_id INT,
-    titulo VARCHAR(150),
-    status VARCHAR(20),
-    prioridade VARCHAR(20),
-    data_abertura DATE,
+    cliente_id INT NOT NULL,
+    titulo VARCHAR(150) NOT NULL,
+    status VARCHAR(20) NOT NULL DEFAULT 'Aberto',
+    prioridade VARCHAR(20) NOT NULL DEFAULT 'Média',
+    data_abertura DATE NOT NULL,
     FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
